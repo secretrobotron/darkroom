2fbe22a008b2fade33c30efec089d097d8773aa1
secretrobotron <secretrobotron@gmail.com>
2012-11-28 11:52:49 -0500

    configuring config & knox

---
+++ README.md
@@ -0,0 +1,3 @@
+# Darkroom
+
+A 3D anaglyph creation web-app.
--- server.js
+++ server.js
@@ -1,7 +1,14 @@
-var express = require("express"),
+var express = require('express'),
+    knox = require('knox'),
+    config = require('config'),
     app = express();
 
 app.use(express.logger())
-   .use(express.static(__dirname + "/public"));
+   .use(express.static(__dirname + '/public'));
+
+console.log(config);
+
+
+var knoxClient = knox.createClient();
 
 app.listen(8080);
