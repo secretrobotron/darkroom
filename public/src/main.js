8368f7452fdde6f5d828d7d26f166d8e2d6499c6
Kate Hudson <katehudsondesign@gmail.com>
2012-11-29 16:28:59 -0500

    mobile styles and methods and stuff

--- public/src/main.js
+++ public/src/main.js
@@ -1,18 +1,25 @@
 (function(){
-  
+
   require(['input-canvas', 'render-canvas'], function(InputCanvas, RenderCanvas){
 
-    var dropFunction = function(){};
+    var readFunction = function(){};
+
+    var fileInput;
+    var cover;
 
     function start(){
+      var canvases = document.querySelector('.canvases');
       var inputContainers = document.querySelectorAll('div.canvas-container.input');
       var outputContainer = document.querySelector('div.canvas-container.output');
       var outputCanvas = outputContainer.querySelector('canvas');
       var reverseButton = document.querySelector('.reverse');
       var saveButton = document.querySelector('.save');
       var mainContainer = document.querySelector('div.main');
+      var fileInput = document.querySelector('.file-input');
+      var cover = document.querySelector('.cover');
       var currentInputCanvas;
       var afterDropFunction;
+      var coverClick;
 
       function render(){
         renderCanvas.render(inputCanvas1, inputCanvas2, inputCanvas1.offset[0], inputCanvas1.offset[1], inputCanvas2.offset[0], inputCanvas2.offset[1]);
@@ -33,10 +40,15 @@
         afterDropFunction = afterDrop2;
         inputCanvas1.deactivateMouseControls();
         inputCanvas2.activateMouseControls();
+        cover.classList.add('dropped-1');
       }
 
       function afterDrop2(){
         inputContainers[1].classList.remove('empty');
+        cover.removeEventListener('click', coverClick, false);
+        cover.style.display = "none";
+        document.querySelector('.photos').classList.add('disabled');
+        document.querySelector('.move').classList.remove('disabled');
       }
 
       afterDropFunction = afterDrop1;
@@ -54,9 +66,7 @@
         render();
       }, false);
 
-      dropFunction = function(e){
-        var file = e.dataTransfer.files[0];
-
+      readFunction = function(file){
         var reader = new FileReader();
         reader.onload = function(e){
           var img = new Image();
@@ -68,8 +78,18 @@
           img.src = reader.result;
         };
 
-        reader.readAsDataURL(file);        
+        reader.readAsDataURL(file);
       };
+
+      coverClick = function() {
+        fileInput.click();
+      }
+
+      cover.addEventListener('click', coverClick, false);
+
+      fileInput.addEventListener('change', function(e){
+        readFunction(e.target.files[0]);
+      }, false);
 
       saveButton.addEventListener('click', function(e){
         var xhr = new XMLHttpRequest();
@@ -78,7 +98,7 @@
         xhr.open('POST', '/save', true);
         xhr.onreadystatechange = function(e){
           if(e.target.readyState === 4){
-            console.log(e.target.response);  
+            console.log(e.target.response);
           }
         };
         xhr.setRequestHeader('Content-Type', 'image/jpeg');
@@ -97,7 +117,7 @@
     document.addEventListener('drop', function(e){
       e.preventDefault();
       e.stopPropagation();
-      dropFunction(e);
+      readFunction(e.dataTransfer.files[0]);
       return false;
     }, false);
 
